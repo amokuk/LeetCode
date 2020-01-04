a9d144c4dba97f9f126f8d9f2e559366a63d304a
amokuk <liuchengtc@gmail.com>
2020-01-04 02:58:17 -0500

    No. 7 Solution

---
+++ src/com/leetcode/easy/seven/Solution7.java
@@ -0,0 +1,24 @@
+package com.leetcode.easy.seven;
+
+public class Solution7 {
+    public int reverse(int x) {
+        int ret = 0;
+        while(x != 0) {
+            int temp = x % 10;
+            x /= 10;
+
+            if(ret > (Integer.MAX_VALUE / 10) || ret < (Integer.MIN_VALUE / 10)) {
+                return 0;
+            }
+
+            if((ret == (Integer.MAX_VALUE / 10) && temp > 7) || (ret == (Integer.MIN_VALUE / 10) && temp < -8)) {
+                return 0;
+            }
+
+            ret *= 10;
+            ret += temp;
+        }
+
+        return ret;
+    }
+}
